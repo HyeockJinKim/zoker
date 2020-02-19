08164ece68e430fa62b4c68824a5d6d284b8bfee
HyeockJinKim <kherootz@gmail.com>
2020-02-19 17:30:51 +0900

    Travis 적용
    CI를 위한 travis CI 적용

---
+++ README.md
@@ -0,0 +1,5 @@
+# Zoker
+> The zoker is a tool you can use to apply `zero knowledge proofs` to smart contracts.
+
+![travis-ci](https://travis-ci.org/HyeockJinKim/zoker-parser.svg?branch=master)
+
--- tests/test_location.rs
+++ tests/test_location.rs
@@ -7,3 +7,28 @@ fn test_location_new() {
     assert_eq!(loc.row(), 3);
     assert_eq!(loc.column(), 7);
 }
+
+#[test]
+fn test_location_reset() {
+    let mut loc = Location::new(3, 7);
+    loc.reset();
+    assert_eq!(loc.row(), 1);
+    assert_eq!(loc.column(), 1);
+}
+
+#[test]
+fn test_location_go_right() {
+    let mut loc = Location::new(3, 7);
+    loc.go_right();
+    assert_eq!(loc.row(), 3);
+    assert_eq!(loc.column(), 8);
+}
+
+#[test]
+fn test_location_new_line() {
+    let mut loc = Location::new(3, 7);
+    loc.new_line();
+    assert_eq!(loc.row(), 4);
+    assert_eq!(loc.column(), 1);
+}
+
