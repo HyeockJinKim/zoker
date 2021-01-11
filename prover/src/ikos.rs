bacdc1133d9742352332057f76ae4b05143409b4
HyeockJinKim <kherootz@gmail.com>
2021-01-11 15:38:51 +0900

    Ikos Verification 구현

--- prover/src/ikos.rs
+++ prover/src/ikos.rs
@@ -17,6 +17,8 @@ macro_rules! set_bit {
     }};
 }
 
+pub type IKosResult<T> = Result<T, IKosError>;
+
 #[derive(Debug, PartialEq)]
 pub struct IKosError {
     pub error: String,
@@ -44,6 +46,13 @@ pub struct IKosVariable4P {
     ctx: Vec<IKosContext>,
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct IKosVariable4V {
+    value: Vec<u32>,
+    ctx: Vec<IKosContext>,
+    inst_random: Vec<u32>,
+}
+
 fn generate_random(num: usize) -> Vec<u8> {
     // TODO: Use Random
     vec![0; num]
@@ -55,7 +64,7 @@ fn generate_all_randomness(rand_len: usize) -> Vec<u32> {
     vec![0; len]
 }
 
-fn get_next_random_from_context(ctx: &mut IKosContext) -> Result<u32, IKosError> {
+fn get_next_random_from_context(ctx: &mut IKosContext) -> IKosResult<u32> {
     if ctx.randomness.len() as u32 <= ctx.used_rand_ctr {
         return Err(IKosError {
             error: String::from("All pre-generated randomness are exhausted!"),
@@ -141,6 +150,13 @@ impl IKosVariable4P {
 
     fn copy_context(&mut self, rhs_ctx: Vec<IKosContext>) {
         self.ctx = rhs_ctx;
+    }
+
+    pub fn negate(mut self) -> Self {
+        for i in 0..3 {
+            self.value[i] = !self.value[i];
+        }
+        self
     }
 
     pub fn or(mut self, rhs: &IKosVariable4P) -> Self {
@@ -252,5 +268,246 @@ impl IKosVariable4P {
             self.ctx[i].ikos_view.out_data32.push(out[i]);
         }
         self
+    }
+}
+
+impl IKosVariable4V {
+    pub fn new() -> Self {
+        IKosVariable4V {
+            value: vec![0; 3],
+            ctx: vec![],
+            inst_random: vec![],
+        }
+    }
+
+    pub fn new_value(value: u32) -> Self {
+        IKosVariable4V {
+            value: vec![value; 3],
+            ctx: vec![],
+            inst_random: vec![],
+        }
+    }
+
+    pub fn new_share(value: Vec<u32>, ctx: Vec<IKosContext>) -> Self {
+        IKosVariable4V {
+            value,
+            ctx,
+            inst_random: vec![],
+        }
+    }
+
+    pub fn new_share_with_random(
+        value: Vec<u32>,
+        ctx: Vec<IKosContext>,
+        inst_random: Vec<u32>,
+    ) -> Self {
+        IKosVariable4V {
+            value,
+            ctx,
+            inst_random,
+        }
+    }
+
+    // TODO: 이 함수들은 P 에서도 사용함
+    fn is_empty_context(&self) -> bool {
+        self.ctx.is_empty()
+    }
+
+    // TODO: 이 함수들은 P 에서도 사용함
+    fn copy_context(&mut self, rhs_ctx: Vec<IKosContext>) {
+        self.ctx = rhs_ctx;
+    }
+
+    pub fn negate(mut self) -> Self {
+        for i in 0..3 {
+            self.value[i] = !self.value[i];
+        }
+        self
+    }
+
+    pub fn or(mut self, rhs: &IKosVariable4V) -> Self {
+        if self.is_empty_context() {
+            self.copy_context(rhs.ctx.clone());
+        }
+        for i in 0..3 {
+            self.value[i] ^= rhs.value[i];
+        }
+        self
+    }
+
+    pub fn rshift(mut self, n: u32) -> Self {
+        for i in 0..3 {
+            self.value[i] >>= n;
+        }
+        self
+    }
+
+    pub fn lshift(mut self, n: u32) -> Self {
+        for i in 0..3 {
+            self.value[i] <<= n;
+        }
+        self
+    }
+
+    fn require_reconstruct(ctx: &Vec<IKosContext>) -> bool {
+        ctx[0].ikos_view.out_data32.len() == ctx[1].ikos_view.out_data32.len()
+    }
+
+    fn get_next_random(&mut self, i: usize) -> IKosResult<u32> {
+        if i < 2 {
+            get_next_random_from_context(&mut self.ctx[i])
+        } else {
+            Ok(0)
+        }
+    }
+
+    pub fn bit_and(mut self, rhs: &IKosVariable4V) -> IKosResult<Self> {
+        let mut rand = vec![0; 3];
+        let mut out = 0;
+
+        if self.is_empty_context() && rhs.is_empty_context() {
+            for i in 0..3 {
+                self.value[i] &= rhs.value[i];
+            }
+            return Ok(self);
+        }
+
+        if self.is_empty_context() {
+            self.copy_context(rhs.ctx.clone());
+        }
+
+        for i in 0..3 {
+            rand[i] = get_next_random_from_context(&mut self.ctx[i]).unwrap();
+        }
+
+        out = (self.value[0] & rhs.value[1])
+            ^ (self.value[1] & rhs.value[0])
+            ^ (self.value[0] & rhs.value[0])
+            ^ rand[0]
+            ^ rand[1];
+
+        if self.ctx[0].is_verify_mode {
+            // verify mode
+            if !IKosVariable4V::require_reconstruct(&self.ctx) {
+                if out != self.ctx[0].ikos_view.out_data32[self.ctx[0].out_view_ctr32] {
+                    return Err(IKosError {
+                        error: String::from("_IkosVariable4V & operation fail."),
+                    });
+                }
+            } else {
+                self.ctx[0].ikos_view.out_data32.push(out);
+            }
+            self.value[0] = out;
+            self.value[1] = self.ctx[1].ikos_view.out_data32[self.ctx[1].out_view_ctr32];
+            for i in 0..3 {
+                self.ctx[i].out_view_ctr32 += 1;
+            }
+        } else {
+            // Non verify mode
+            self.value[0] = out;
+            self.value[1] = self.inst_random[self.inst_random[0]];
+            self.inst_random[0] += 1;
+
+            for i in 0..3 {
+                self.ctx[i].ikos_view.out_data32.push(self.value[i]);
+            }
+        }
+
+        Ok(self)
+    }
+
+    pub fn bit_or(mut self, rhs: &IKosVariable4V) -> Self {
+        if self.is_empty_context() {
+            self.copy_context(rhs.ctx.clone());
+        }
+        for i in 0..3 {
+            self.value[i] |= rhs.value[i];
+        }
+        self
+    }
+
+    pub fn add(mut self, rhs: &IKosVariable4V) -> IKosResult<Self> {
+        let mut a = vec![0; 3];
+        let mut b = vec![0; 3];
+        let mut c = 0;
+        let mut rand = vec![0; 3];
+        let mut out = vec![0; 3];
+
+        if self.is_empty_context() && rhs.is_empty_context() {
+            for i in 0..3 {
+                self.value[i] += rhs.value[i];
+            }
+            return Ok(self);
+        }
+
+        if self.is_empty_context() {
+            self.copy_context(rhs.ctx.clone());
+        }
+        for i in 0..3 {
+            rand[i] = self.get_next_random(i)?;
+        }
+
+        if self.ctx[0].is_verify_mode {
+            // verify mode
+            let required = IKosVariable4V::require_reconstruct(&self.ctx);
+            for i in 0..3 {
+                if !required || i != 0 {
+                    out[i] = self.ctx[i].ikos_view.out_data32[self.ctx[i].out_view_ctr32];
+                }
+                self.ctx[i].out_view_ctr32 += 1;
+            }
+            for i in 0..31 {
+                for j in 0..3 {
+                    a[j] = get_bit!(self.value[j] ^ out[j], i);
+                    b[j] = get_bit!(rhs.value[j] ^ out[j], i);
+                }
+                c = (a[0] & b[1]) ^ (a[1] & b[0]) ^ (get_bit!(rand[1], i));
+                if !required {
+                    if (c ^ (a[0] & b[0]) ^ (get_bit!(out[0], i)) ^ (get_bit!(rand[0], i)))
+                        != (get_bit!(out[0], i + 1))
+                    {
+                        return Err(IKosError {
+                            error: String::from("_IkosVariable4V + operation fail."),
+                        });
+                    }
+                } else {
+                    set_bit!(
+                        out[0],
+                        i + 1,
+                        c ^ (a[0] & b[0]) ^ (get_bit!(out[0], i)) ^ (get_bit!(rand[0], i))
+                    );
+                }
+            }
+            if required {
+                self.ctx[0].ikos_view.out_data32.push(out[0]);
+            }
+            for i in 0..3 {
+                self.value[i] = self.value[i] ^ rhs.value[i] ^ out[i];
+            }
+        } else {
+            // Non verify mode
+            out[1] = self.inst_random[self.inst_random[0]];
+            self.inst_random[0] += 1;
+            set_bit!(out[1], 0, 0);
+
+            for i in 0..31 {
+                for j in 0..3 {
+                    a[j] = get_bit!(self.value[j] ^ out[j], i);
+                    b[j] = get_bit!(rhs.value[j] ^ out[j], i);
+                }
+                c = (a[0] & b[1]) ^ (a[1] & b[0]) ^ (get_bit!(rand[1], i));
+                set_bit!(
+                    out[0],
+                    i + 1,
+                    c ^ (a[0] & b[0]) ^ (get_bit!(out[0], i)) ^ (get_bit!(rand[0], i))
+                );
+            }
+
+            for i in 0..3 {
+                self.value[i] = self.value[i] ^ rhs.value[i] ^ out[i];
+                self.ctx[i].ikos_view.out_data32.push(out[i]);
+            }
+        }
+        Ok(self)
     }
 }
--- prover/tests/test_prover.rs
+++ prover/tests/test_prover.rs
@@ -2,7 +2,12 @@ use zoker_prover::ikos::IKosVariable4P;
 
 #[test]
 fn test_prover() {
-    let a = IKosVariable4P::new();
-    let b = IKosVariable4P::new();
-    a.clone().bit_and(&b).bit_and(&a);
+    let a = IKosVariable4P::new_value(1);
+    let b = IKosVariable4P::new_value(2);
+    println!("{:#?}", a);
+    println!("{:#?}", a.clone().bit_and(&b).bit_and(&a));
+    println!("{:#?}", a);
+
+    println!("{:#?}", a.clone().negate());
+    println!("{:#?}", a);
 }
